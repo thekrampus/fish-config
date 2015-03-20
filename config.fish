1b07e884a0069d4ebece95afedce548a276c1156
rob <rkelly@nmt.edu>
2015-03-20 02:57:52 -0600

    Revised color palette to fit my awesome theme a little better

--- config.fish
+++ config.fish
@@ -30,5 +30,8 @@ if status --is-login
 end
 
 ## Rotating rainbow palette because it's the 90s
-set -g rainbow 97080e da4b0f e9b104 488c13 1b55c0
+# This is a more "skittles" flavoured palette
+#set -g rainbow 97080e da4b0f e9b104 488c13 1b55c0
+# This is a more "seafoam" flavoured pallete. It fits my WM theme a little better.
+set -g rainbow 346767 488d8d 93c8c8 badbdb 6ab3b3 3c7777
 set -g static_rainbow $rainbow
