16c73b7274fe1a6b9036da8ba8956e9cb0228d2e
Rob Kelly <contact@robkel.ly>
2019-04-18 15:39:25 -0600

    Added some aliases & environment conf useful enough that it ought to be shared

--- alias.fish
+++ alias.fish
@@ -1,7 +1,22 @@
 ### Set aliases
-alias ls='ls --color=auto'
+alias ls="ls --color=auto"
 alias ll="ls -alh"
 alias rmacs="rm -f {*~,\#*\#}"
+alias g="git"
+
+alias emacs="emacs -nw"
+alias xemacs="command emacs"
+alias cemacs="emacsclient -t"
+
+alias matlab="matlab -nodesktop -nosplash"
+alias xmatlab="command matlab"
+
+alias octave="octave --no-gui"
+alias xoctave="command octave"
+
+function urxvt_clone
+    urxvt -cd $PWD &
+end
 
 ## Load local aliases
 . ~/.config/fish/local.d/alias.fish
--- env.fish
+++ env.fish
@@ -1,6 +1,9 @@
 ### Set environment variables
 setenv LANG en_US.UTF-8
 setenv EDITOR emacs
+setenv VIRTUAL_ENV_DISABLE_PROMPT 1
+setenv LESS "--RAW-CONTROL-CHARS"
+setenv LESSOPEN "| highlight --line-numbers --force -O xterm256 -s darkspectrum %s"
 
 setenv PATH $HOME/bin /usr/sbin /sbin /usr/local/sbin $PATH
 
