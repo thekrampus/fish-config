b493bfed4f1f17a20a9e2b97f0c70c00209b255c
Rob Kelly <contact@robkel.ly>
2019-04-18 15:07:39 -0600

    Prompt enhancements

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -15,33 +15,59 @@ set __fish_prompt_hookup_glyph   '╭⊸'
 set __fish_prompt_hookdown_glyph '╰⊸'
 set __fish_prompt_hook_color     $static_rainbow[4]
 
+set __fish_prompt_bad_status_glyph '⌥'
+set __fish_prompt_bad_status_color brred
+
+set __fish_venv_prompt_glyph '⚕'
+
 ## Should look something like this...
-# ╭⊸[rob@starman ~/.c/fish ]「 master↑1|✔」
+# ╭⊸[rob@starman ~/.c/fish ]「 master↑1|✔」{ ⚕ venv }
 # ╰⊸ echo "hey imma computah! stop all th' downloadin'!"
 
 function fish_prompt
+    set p_status $status
     if test $TERM = "dumb"
         echo "\$"
     else
-        set_color $__fish_prompt_hook_color
-        echo -n $__fish_prompt_hookup_glyph
-        set_color normal
-        echo -n "[$USER@"(hostname) (prompt_pwd)" ]"
+        # Line 1
+        set p_hookup (set_color $__fish_prompt_hook_color)"$__fish_prompt_hookup_glyph"(set_color normal)
+
+        set p_info "[$USER@"(hostname) (prompt_pwd)" ]"
+        if test $p_status -ne 0
+            set p_info (set_color $__fish_prompt_bad_status_color)"$p_info"(set_color normal)
+        end
 
         set branch (__fish_git_prompt | sed s/[\(\)]//g)
         if test -z $branch
             set branch (set_color $rainbow[1])"☯"(set_color normal)
             set -g rainbow $rainbow[2..-1 1]
         end
-        echo -n "「$branch」"
+        set p_branch "「$branch」"
 
         if test -n "$VIRTUAL_ENV"
-            echo -n "{" (basename "$VIRTUAL_ENV") "}"
+            set p_venv "{ $__fish_venv_prompt_glyph"(basename "$VIRTUAL_ENV") "}"
         end
 
-        echo
-        set_color $__fish_prompt_hook_color
-        echo -n $__fish_prompt_hookdown_glyph
-        set_color normal
+        set p_line_1 "$p_hookup$p_info$p_branch$p_venv"
+
+        # Line 2
+        set p_hookdown (set_color $__fish_prompt_hook_color)"$__fish_prompt_hookdown_glyph"(set_color normal)
+
+        set p_line_2 "$p_hookdown"
+
+        # Write out
+        echo "$p_line_1"
+        echo "$p_line_2"
+    end
+end
+
+function fish_right_prompt -d "Write out the right-aligned prompt"
+    set p_status $status
+    if test $TERM != "dumb"
+        if test $p_status -ne 0
+            set_color $__fish_prompt_bad_status_color
+            echo $__fish_prompt_bad_status_glyph $prompt_status
+            set_color normal
+        end
     end
 end
